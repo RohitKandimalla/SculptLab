fee53619fcb61bb933a44029ceddbb8bada0297f
Rohit Kandimalla <rohitkandimalla@Rohits-MacBook-Air.local>
2024-02-01 22:29:29 -0500

    first commit

---
+++ src/main/java/net/sculptLab/SculptLabApplication.java
@@ -0,0 +1,13 @@
+package net.sculptLab.sculptLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SculptLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SculptLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/sculptLab/sculptLab/SculptLabApplicationTests.java
@@ -0,0 +1,13 @@
+package net.sculptLab.sculptLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SculptLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
